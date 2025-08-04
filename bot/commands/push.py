b9e4b39ad5bfb40ffbb21f4f9967e351319a21e7
Liescake <2899432715@qq.com>
2025-08-04 19:27:01 +0800

    Init commit

---
+++ bot/commands/push.py
@@ -0,0 +1,9 @@
+import subprocess
+import logging
+from pathlib import Path
+
+class Command:
+    def __init__(self):
+        pass
+    def _run_git(self,args):
+        pass
--- bot/controller.py
+++ bot/controller.py
@@ -1,11 +1,16 @@
-# 主控制模块import 
+# 主控制模块
 import logging
 from logger import init_logger
+import commands
 
 
 class BotColtroller:
     def __init__(self):
         init_logger()
+        logging.info("Controller start instantiating")
+        self.commands = {
+            "status":self.run("status")
+        }
         logging.info("Controller is instantiated")
-    def run(self):
+    def run(self,command):
         pass
